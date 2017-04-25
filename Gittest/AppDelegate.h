be8d2b49663e1669af09b4011568c2aa4c4f079a
APPLE <APPLE@APPLEs-MacBook-Pro.local>
2017-04-25 14:48:03 +0530

    Initial Commit

---
+++ Gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gittest
+//
+//  Created by APPLE on 25/04/17.
+//  Copyright © 2017 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gittest
+//
+//  Created by APPLE on 25/04/17.
+//  Copyright © 2017 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
